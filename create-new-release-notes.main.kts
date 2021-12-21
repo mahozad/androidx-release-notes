42a2a13a88bc479f58617fbff1ce211bce36d8e7
Mahdi Hosseinzadeh <mdihosseinzadeh@gmail.com>
2021-12-21 16:10:21 +0330

    Add some Kdoc for a function

--- create-new-release-notes.main.kts
+++ create-new-release-notes.main.kts
@@ -37,6 +37,15 @@ val reader = tryToGet(
 
 // TODO: Duplicate; use the try-to-get.main.kts script.
 //  See other scripts for example usage.
+/**
+ * We could also make subsequent runs wait
+ * for an [exponential delay](https://en.wikipedia.org/wiki/Exponential_backoff).
+ * See [this article](https://dzone.com/articles/understanding-retry-pattern-with-exponential-back).
+ *
+ * I wrote this function myself.
+ * It is interesting that [this solution](https://stackoverflow.com/a/46890009)
+ * proposed by Roman Elizarov is very similar to mine.
+ */
 fun <T> tryToGet(
     block: () -> T,
     retryCount: Int,
