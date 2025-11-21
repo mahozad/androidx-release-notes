4ed892e29ba918813eaf90f9d4a6cf4935eb65d3
Mahdi Hosseinzadeh <mdihosseinzadeh@gmail.com>
2025-11-21 09:58:41 +0330

    Refactor the mouse movement generator script

--- scripts/mouse.main.kts
+++ scripts/mouse.main.kts
@@ -8,6 +8,8 @@ import kotlin.math.roundToInt
 
 typealias Point = Pair<Int, Int>
 
+val resultFile = Path("mouse-movements.arf")
+
 // Target cursor points
 val center = 640 to 450
 val watch = 948 to 222
@@ -15,51 +17,44 @@ val custom = 958 to 480
 val release = 456 to 312
 val apply = 818 to 456
 
-val waitAtCenter = interpolate(center, center, 60).map(::moveTo)
-val moveFromCenterToWatch = interpolate(center, watch, 30).map(::moveTo)
-val waitAtWatch = interpolate(watch, watch, 15).map(::moveTo)
-val moveFromWatchToCustom = interpolate(watch, custom, 30).map(::moveTo)
-val waitAtCustom = interpolate(custom, custom, 15).map(::moveTo)
-val moveFromCustomToRelease = interpolate(custom, release, 30).map(::moveTo)
-val waitAtRelease = interpolate(release, release, 15).map(::moveTo)
-val moveFromReleaseToApply = interpolate(release, apply, 30).map(::moveTo)
-val waitAtApply = interpolate(apply, apply, 15).map(::moveTo)
-val moveFromApplyToCenter = interpolate(apply, center, 30).map(::moveTo)
-
 (
-    waitAtCenter +
-    moveFromCenterToWatch + waitAtWatch + press(watch) + release(watch) + waitAtWatch +
-    moveFromWatchToCustom + waitAtCustom + press(custom) + release(custom) + waitAtCustom +
-    moveFromCustomToRelease + waitAtRelease + press(release) + release(release) + waitAtRelease +
-    moveFromReleaseToApply + waitAtApply + press(apply) + release(apply) + waitAtApply + waitAtApply +
-    moveFromApplyToCenter + waitAtCenter
+    waitAt(center, duration = 60) +
+    move(from = center, to = watch) + waitAt(watch) + press(watch) + letGo(watch) + waitAt(watch) +
+    move(from = watch, to = custom) + waitAt(custom) + press(custom) + letGo(custom) + waitAt(custom) +
+    move(from = custom, to = release) + waitAt(release) + press(release) + letGo(release) + waitAt(release) +
+    move(from = release, to = apply) + waitAt(apply) + press(apply) + letGo(apply) + waitAt(apply, duration = 30) +
+    move(from = apply, to = center) + waitAt(center, duration = 60)
 )
     .joinToString(separator = "\n")
-    .let { Path("mouse-movements.arf").writeText(it) }
+    .let(resultFile::writeText)
+
+fun waitAt(point: Point, duration: Int = 15) = interpolate(point, point, duration).map(::goTo)
+
+fun move(from: Point, to: Point) = interpolate(from, to, 30).map(::goTo)
+
+fun press(point: Point) = "${point.first}|${point.second}|ltd"
+
+fun letGo(point: Point) = "${point.first}|${point.second}|ltu"
+
+fun goTo(point: Point) = "${point.first}|${point.second}|mov"
 
 fun interpolate(
     a: Point,
     b: Point,
-    count: Int
+    steps: Int
 ): List<Point> {
     val (x1, y1) = a
     val (x2, y2) = b
     val deltaX = x2 - x1
     val deltaY = y2 - y1
-    val gradient = if (deltaX == 0) 0f else deltaY.toFloat() / deltaX
-    val xIncrement = deltaX.toFloat() / count
-    val yIncrement = xIncrement * gradient
+    val slope = if (deltaX == 0) 0f else deltaY / deltaX.toFloat()
+    val xIncrement = deltaX.toFloat() / steps
+    val yIncrement = xIncrement * slope
     return buildList {
-        for (i in 0..count) {
-            val x = x1 + i * xIncrement
-            val y = y1 + i * yIncrement
+        for (i in 0..steps) {
+            val x = x1 + (i * xIncrement)
+            val y = y1 + (i * yIncrement)
             add(x.roundToInt() to y.roundToInt())
         }
     }
 }
-
-fun moveTo(point: Point) = "${point.first}|${point.second}|mov"
-
-fun press(point: Point) = "${point.first}|${point.second}|ltd"
-
-fun release(point: Point) = "${point.first}|${point.second}|ltu"
